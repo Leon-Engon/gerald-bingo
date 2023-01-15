b7b4d06ff1c71c54737c2b507399e33d84cce33f
Leon-Engon <122726181+Leon-Engon@users.noreply.github.com>
2023-01-15 12:10:36 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,6 +23,11 @@ let Atzar = 0
 let Mostra = 0
 let Llistatnumeros: number[] = []
 let list: number[] = []
+basic.showIcon(IconNames.Diamond)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.showIcon(IconNames.SmallDiamond)
+basic.pause(100)
+basic.clearScreen()
 let Bolas = 89
 for (let index = 0; index <= Bolas - 1; index++) {
     list.push(index + 1)
